8c0506a447afb6377229cbb9b6696927d39d2e4e
MangoAbricosik <70761769+MangoAbricosik@users.noreply.github.com>
2020-10-17 01:38:11 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp112321321121212/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Solo_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sus, kol, ots;
+            try
+            {
+                Console.WriteLine("Сколько нужно пиромид - ");
+                sus = int.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                sus = 10000;
+            }
+            try
+            {
+                
+                Console.WriteLine("Высота пирамид - ");
+                kol = int.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                kol = 230;
+            }
+            try
+            {
+                Console.WriteLine("Отступ пирамид - ");
+                ots = int.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                ots = 0;
+            }
+            Console.Clear();
+
+            int count = 1;
+
+            for (int i = 0; i<sus;i++)
+            {
+                for (int q = 0; q<kol-11;q++)
+                {
+                    Console.Write("#");
+                }
+                kol--;
+                for (int q = 0;q<10;q++)
+                {
+                    Console.Write(" ");
+                }
+                for (int q = 0;q<count;q++)
+                {
+                    Console.Write("#");
+                }
+                count++;
+            }
+        }
+    }
+}
